c4a501fb2a47b13b543022b74f26cb56858e005b
ArshiA <letmemakenewone@gmail.com>
2019-04-16 12:57:24 +0430

    test

--- duelyst/src/Model/card/hermione/AttackType.java
+++ duelyst/src/Model/card/hermione/AttackType.java
@@ -3,6 +3,7 @@ package Model.card.hermione;
 import Model.card.Card;
 
 public interface AttackType {
+    //dasfjasdhfkjhsadkjfhakjsdfdkfnjak
     void counterAttack(Card enemyCard);
     void attack(Card enemyCard );
 }
