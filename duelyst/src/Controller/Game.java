2864f1eeb0c8ecfde78ef63dc867750c20f7cf70
fateme <fatt3me@gmail.com>
2019-04-27 19:52:17 +0430

    git -_-

--- duelyst/src/Controller/Game.java
+++ duelyst/src/Controller/Game.java
@@ -5,7 +5,7 @@ import Model.account.Account;
 import Controller.menu.Menu;
 
 public class Game {
-    public static Account[] account=new Account[2];
+    public static Account[] account = new Account[2];
     public static Menu menu;
     public static Battle battle;
 
