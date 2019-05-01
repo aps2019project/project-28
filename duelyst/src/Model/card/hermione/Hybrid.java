7971fe062a6759417d4158876451438dc8465eca
ArshiA <letmemakenewone@gmail.com>
2019-05-01 15:00:44 +0430

    HyBrid bug fixing
    Written By ArshiA
    
    note:
    
    fixing class Hybrid's Dictation

--- duelyst/src/Model/card/hermione/Hybrid.java
+++ duelyst/src/Model/card/hermione/Hybrid.java
@@ -3,7 +3,7 @@ package Model.card.hermione;
 import Model.Map.Map;
 import Model.card.Card;
 
-public class Hybreed implements AttackType {
+public class Hybrid implements AttackType {
     @Override
     public boolean canReach(Hermione champCard, Hermione enemyCard) {
         if(Map.getManhattanDistance(champCard.getLocation(),enemyCard.getLocation())<=champCard.getRange())return true;
