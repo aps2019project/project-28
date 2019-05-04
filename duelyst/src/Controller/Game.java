104ad080e341c79da83247573d74b8ca93de34f4
fateme <fatt3me@gmail.com>
2019-05-05 02:22:44 +0430

    Name account[] Fixed

--- duelyst/src/Controller/Game.java
+++ duelyst/src/Controller/Game.java
@@ -4,7 +4,7 @@ import Model.account.Account;
 import Controller.menu.Menu;
 
 public class Game {
-    public static Account[] account = new Account[2];
+    public static Account[] accounts = new Account[2];
     public static Menu menu;
     public static Battle battle;
 
