28a80ffb7a7c41b803506c2945b9cddb7c010705
ArshiA <letmemakenewone@gmail.com>
2019-05-05 22:09:01 +0430

    menu
    Written By ArshiA
    
    note:

--- duelyst/src/Controller/menu/GraveYardMenu.java
+++ duelyst/src/Controller/menu/GraveYardMenu.java
@@ -1,29 +1,9 @@
 package Controller.menu;
 
-import java.util.ArrayList;
-
 public class GraveYardMenu extends Menu {
-
-    private ArrayList<OnGraveYardInfoClickedListener> graveYardInfoClickedListeners;
-    private ArrayList<OnGraveYardCardInfoClickedListener> graveYardCardInfoClickedListeners;
-
     public GraveYardMenu(Menu parentMenu) {
         super(parentMenu);
-        this.account = parentMenu.getAccount();
-    }
-
-    void showInfo(int cardid){
-        for (OnGraveYardCardInfoClickedListener presenter:
-             graveYardCardInfoClickedListeners) {
-            presenter.show(account.getCollection(), cardid);
-        }
-    }
-
-    void showCards(){
-        for (OnGraveYardInfoClickedListener presenter:
-                graveYardInfoClickedListeners) {
-            presenter.Show(account.getCollection());
-        }
+        this.account=parentMenu.getAccount();
     }
 
     @Override
@@ -31,9 +11,8 @@ public class GraveYardMenu extends Menu {
 
     }
 
-    // TODO: 5/5/19 grave Yard
-    
     public void showInfo(int cardID){
+
     }
 
 }
