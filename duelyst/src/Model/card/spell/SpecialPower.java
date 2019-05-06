9f33059be839534bb837984e79d15d1c387a9059
fateme <fatt3me@gmail.com>
2019-05-06 12:32:26 +0430

    hardCode ok

--- duelyst/src/Model/card/spell/SpecialPower.java
+++ duelyst/src/Model/card/spell/SpecialPower.java
@@ -8,8 +8,4 @@ public class SpecialPower extends Spell {
     public SpecialPower(String name, int price, int manaPoint, int duration , int perk, Target target, Action... actions) {
         super(name, price, manaPoint, duration, perk, target, actions);
     }
-
-    public SpecialPower(SpecialPower specialPower){
-        super(specialPower);
-    }
 }
