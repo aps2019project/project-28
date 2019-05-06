3c16cb772819bcfb31a4d914104e801dac9a7f2c
Siniorss <s.saadat@student.sharif.edu>
2019-05-06 12:07:42 +0430

    handled some more item actions again and again and again

--- duelyst/src/Model/card/spell/Targets/Targetwithin2ManhattanDistance.java
+++ duelyst/src/Model/card/spell/Targets/Targetwithin2ManhattanDistance.java
@@ -3,14 +3,12 @@ package Model.card.spell.Targets;
 import Controller.Game;
 import Model.Map.Cell;
 import Model.Map.Map;
-import Model.account.Player;
-import Model.card.spell.Spell;
 import Model.card.spell.Target;
 import exeption.InvalidCellException;
 
 import static java.lang.StrictMath.abs;
 
-public class TargetTwoByTwo implements Target {
+public class Targetwithin2ManhattanDistance implements Target {
     private static TargetTwoByTwo obj;
 
     public static TargetTwoByTwo getTargetInstance() {
@@ -24,14 +22,13 @@ public class TargetTwoByTwo implements Target {
         int x = cell.getX();
         int y = cell.getY();
         Map map = Game.battle.getMap();
-        if (x == Map.WIDTH || y == Map.HEIGHT) {
-            throw new InvalidCellException();
+        Cell[] cells = new Cell[12] ;
+        int index = 0 ;
+        for (int i = -2 ; i <= 2 ; i++) {
+            for (int j = 0 ; abs(j+i) <= 2 ; j++){
+                if (j==0 && i == 0) continue;
+                cells[index] = map.getCell(x+i , y + j) ;
+            }
         }
-        Cell[] cells = {cell, map.getCell(x, y + 1), map.getCell(x + 1, y), map.getCell(x + 1, y + 1)};
-        return cells ;
     }
 }
-
-
-
-
