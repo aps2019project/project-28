b1bb323b6dd31f6c1a9a139a08e593ce5f45c948
Siniorss <s.saadat@student.sharif.edu>
2019-05-15 17:49:19 +0430

    SPECIAL POWER BABYYYY

--- duelyst/src/exeption/InvalidCellException.java
+++ duelyst/src/exeption/InvalidCellException.java
@@ -3,7 +3,7 @@ package exeption;
 public class InvalidCellException extends Exception {
     private final String detailMessage ;
 
-    public InvalidCellException(){ this.detailMessage =  "The cell you have chosen is not valid , Please try again";} ;
+    public InvalidCellException(){ this.detailMessage =  "The cell you have chosen is not valid , Please try again";}
     public InvalidCellException(String message) {this.detailMessage = message ;}
 
         @Override
