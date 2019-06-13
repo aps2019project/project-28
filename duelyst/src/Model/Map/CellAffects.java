ff15a0282419d0a397e70b752312883986980fdd
ArshiA <letmemakenewone@gmail.com>
2019-06-13 18:49:49 +0430

    graphic basis
    written by ArshiA
    
    updates:
            cellAffect graphics

--- duelyst/src/Model/Map/CellAffects.java
+++ duelyst/src/Model/Map/CellAffects.java
@@ -1,5 +1,17 @@
 package Model.Map;
 
+
 public enum CellAffects{
-     fire , poison , holy
+
+     fire(CellAffectGraphics.fire) , poison(CellAffectGraphics.poison) , holy(CellAffectGraphics.holy);
+
+     private CellAffectGraphics graphics;
+
+     CellAffects(CellAffectGraphics graphics) {
+          this.graphics = graphics;
+     }
+
+     public CellAffectGraphics getGraphics(){
+          return this.graphics;
+     }
 }
