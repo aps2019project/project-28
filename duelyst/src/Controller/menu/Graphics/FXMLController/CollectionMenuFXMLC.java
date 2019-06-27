0b5d2ee90b66ea69743be7bbc2244e423bf7ddce
Saee Saadat <saee@samantha.local>
2019-06-27 17:50:13 +0430

    collection menu and shop menu are completely done :)

--- duelyst/src/Controller/menu/Graphics/FXMLController/CollectionMenuFXMLC.java
+++ duelyst/src/Controller/menu/Graphics/FXMLController/CollectionMenuFXMLC.java
@@ -4,17 +4,23 @@ import Controller.menu.Graphics.GraphicsControls;
 import Controller.menu.ShopMenu;
 import Model.card.Card;
 import Model.card.hermione.Hermione;
+import Model.card.spell.Spell;
+import Model.item.Usable;
 import View.MenuHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class CollectionMenuFXMLC extends FXMLController {
@@ -32,9 +38,8 @@ public class CollectionMenuFXMLC extends FXMLController {
         super.buildScene();
         Scene scene = menu.getGraphic().getScene();
         scene.setUserAgentStylesheet("Controller/menu/Graphics/StyleSheets/ShopMenu.css");
-        buildHermionesVbox() ;
+        buildCardsVbox() ;
         scrollPane.setContent(hermionesVbox);
-        buildSpellsVbox();
         buildItemsVbox();
 
         GraphicsControls.setBackButtonOnPress(backButton);
@@ -43,13 +48,12 @@ public class CollectionMenuFXMLC extends FXMLController {
             MenuHandler.goBack();
         });
         setTabPressedStuff();
-
-
-
-
+        updateBalance();
     }
 
-    private void buildHermionesVbox() {
+    private void buildCardsVbox() {
+        List<HBox> hermionehBoxes = new ArrayList<>();
+        List<HBox> spellHboxes = new ArrayList<>();
         for (Card card : menu.getAccount().getCollection().getCards()){
             if (card instanceof Hermione){
                 Hermione h = (Hermione)card ;
@@ -59,22 +63,60 @@ public class CollectionMenuFXMLC extends FXMLController {
                     Parent root = loader.load();
                     CollectionCardHermioneFXMLC fxmlc = loader.getController();
                     fxmlc.buildCardCard(h);
-                    hermionesVbox.getChildren().add(root);
+                    setUpTheHbox(root , hermionesVbox , hermionehBoxes);
                 }catch (IOException ignored) {
                     System.err.println("could'nt load the collectionHermioneCard");
+                }
+            }
+            else if (card instanceof Spell){
+                Spell s = (Spell) card ;
+                FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getClassLoader().getResource(
+                        "Controller/menu/Graphics/FXMLs/CollectionCardSpell.fxml")));
+                try {
+                    Parent root = loader.load();
+                    CollectionCardSpellFXMLC fxmlc = loader.getController();
+                    fxmlc.buildCardCard(s , false);
+                    setUpTheHbox(root , spellsVbox , spellHboxes);
+                }catch (IOException ignored) {
+                    System.err.println("could'nt load the collectionSpellCard");
                 }
             }
         }
     }
 
-    private void buildSpellsVbox() {
-
-    }
 
     private void buildItemsVbox() {
-
+        List<HBox> hBoxes = new ArrayList<>();
+        for (Usable usabel : menu.getAccount().getCollection().getUsables()){
+            FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getClassLoader().getResource(
+                    "Controller/menu/Graphics/FXMLs/CollectionCardItem.fxml")));
+            try {
+                Parent root = loader.load();
+                CollectionCardItemFXMLC fxmlc = loader.getController();
+                fxmlc.buildCardCard(usabel , false);
+                setUpTheHbox(root , itemsVbox , hBoxes);
+            }catch (IOException ignored) {
+                System.err.println("could'nt load the collectionSpellCard");
+            }
+        }
     }
 
+    private void setUpTheHbox(Parent card, VBox vbox , List<HBox> hBoxes) {
+        vbox.setSpacing(15);
+        HBox hbox ;
+        if (hBoxes.size() == 0 || hBoxes.get(hBoxes.size() - 1).getChildren().size() == 5){
+            hbox = new HBox();
+            hbox.setAlignment(Pos.CENTER);
+            hbox.setSpacing(15);
+            hbox.setFillHeight(true);
+            hbox.setPrefWidth(vbox.getWidth());
+            hBoxes.add(hbox);
+            vbox.getChildren().add(hbox);
+        }
+        hbox = hBoxes.get(hBoxes.size()-1);
+        hbox.getChildren().add(card);
+
+    }
 
     private void setTabPressedStuff() {
         tabPressed(hermioneTab);
@@ -84,12 +126,31 @@ public class CollectionMenuFXMLC extends FXMLController {
         hermioneTab.setOnMouseReleased(e -> tabReleased(hermioneTab));
         itemTab.setOnMouseReleased(e -> tabReleased(itemTab));
         spellTab.setOnMouseReleased(e -> tabReleased(spellTab));
+        hermioneTab.setOnAction(e -> {
+            tabPressed(hermioneTab);
+            tabReleased(itemTab);
+            tabReleased(spellTab);
+            scrollPane.setContent(hermionesVbox);
+        });
+        itemTab.setOnAction(e -> {
+            tabPressed(itemTab);
+            tabReleased(hermioneTab);
+            tabReleased(spellTab);
+            scrollPane.setContent(itemsVbox);
+        });
+        spellTab.setOnAction(e -> {
+            tabPressed(spellTab);
+            tabReleased(hermioneTab);
+            tabReleased(itemTab);
+            scrollPane.setContent(spellsVbox);
+        });
     }
 
 
     private void tabPressed(Button tab) {
 //        ShopMenu.getMenu().save() ;
         tab.getStyleClass().add("tab-button-selected");
+        updateBalance();
     }
 
     private void tabReleased(Button tab) {
